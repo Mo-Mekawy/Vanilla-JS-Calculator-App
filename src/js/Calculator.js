bccadac710cd713dcafa33727f171d690a095c3f
mohamed <mekawymohamed@gmail.com>
2024-01-28 12:17:44 +0200

    added the functionality of the calculator class (add, remove, clear, comput and display) features

---
+++ src/js/Calculator.js
@@ -0,0 +1,53 @@
+import beautifyInput from "./helpers/beautifyInput";
+import validateDotAddition from "./helpers/validateDotAddition";
+
+export default class Calculator {
+  constructor(inputEl, outputEl) {
+    this.inputEl = inputEl;
+    this.outputEl = outputEl;
+
+    this.clearAll();
+  }
+
+  comput() {
+    if (this.input === "") return "";
+
+    return eval(this.input);
+  }
+
+  appendInput(val) {
+    if (val === "." && !validateDotAddition(this.input)) return;
+
+    this.input += val;
+    this.inputEl.innerHTML += beautifyInput(val);
+  }
+
+  displayOutput(sum) {
+    this.outputEl.innerText = sum.toLocaleString("en-US");
+  }
+
+  removeLast() {
+    // check if there is an input to remove from
+    if (this.input === "") return;
+
+    this.input = this.input.slice(0, -1); // remove last char
+
+    // remove last child if it were an element (bracket or operator)
+    if (this.inputEl.lastChild.nodeType === 1) {
+      this.inputEl.removeChild(this.inputEl.lastChild);
+      return;
+    }
+
+    // remove the last char if it was a number or dot (text node)
+    this.inputEl.innerHTML = this.inputEl.innerHTML.slice(0, -1);
+  }
+
+  clearAll() {
+    // the input equation and the output num
+    this.input = "";
+    this.output = "";
+
+    this.inputEl.innerHTML = "";
+    this.outputEl.innerText = "";
+  }
+}
