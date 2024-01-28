9cc1fe7d5b88c5525b98d086b09a25b6b771cc9a
mohamed <mekawymohamed@gmail.com>
2024-01-28 10:01:17 +0200

    made a new cursor animation and made the display always show the last input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import "./assets/scss/main.scss";
+import Calculator from "./js/Calculator";
+import scrollOnChange from "./js/animations/inputEl";
+
+// select all needed buttons and display elements
+const clearBtn = document.querySelector("[data-clear]");
+const delBtn = document.querySelector("[data-delete]");
+const equalBtn = document.querySelector("[data-equals]");
+const keyBtns = document.querySelectorAll("[data-key]");
+const inputEL = document.querySelector("[data-input]");
+const outputEl = document.querySelector("[data-output]");
+
+// always make the last input visible
+scrollOnChange(inputEL);
+
+const calculator = new Calculator(inputEL, outputEl);
+
+keyBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    calculator.appendInput(e.target.dataset.key);
+  });
+});
+
+equalBtn.addEventListener("click", () => {
+  calculator.comput();
+  calculator.displayOutput();
+});
+
+clearBtn.addEventListener("click", () => {
+  calculator.clearAll();
+});
+
+delBtn.addEventListener("click", () => {
+  calculator.removeLast();
+});
---
+++ src/js/animations/inputEl.js
@@ -0,0 +1,11 @@
+const scrollToEnd = (el) =>
+  el.scrollTo({ left: el.scrollWidth, behavior: "smooth" });
+
+export default function scrollOnChange(el) {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.type === "childList") scrollToEnd(el);
+    });
+  });
+  observer.observe(el, { childList: true });
+}
