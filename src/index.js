6ed5bd7606f1f7374b3c0153e41c7a3cd1a2da6d
mohamed <mekawymohamed@gmail.com>
2024-01-31 21:01:36 +0200

    added jsdoc and documented Calculator class and app.js and index.js files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+/**
+ * @file index.js is the main entry and the app initiater
+ * @author Mohamed Mekawy <mohamed.ashraf.mekawy@gmail.com>
+ * @see {@link https://mo-mekawy.github.io/Vanilla-JS-Calculator-App/}
+ */
 import "./assets/scss/main.scss";
 import init from "./js/app";
 
--- src/js/animations/scrollOnChange.js
+++ src/js/animations/scrollOnChange.js
@@ -1,3 +1,16 @@
+/**
+ * @module scrollOnChange
+ * @description make an elements last child always visible
+ * by scrolling to the end when any child or text is added
+ */
+
+/**
+ * @description make an elements last child always visible
+ * by scrolling to the end when any child or text is added
+ * @function scrollOnChange
+ * @param {Element} el the element to observe
+ * @returns {MutationObserver} observer so it can disconnect if needed
+ */
 export default function scrollOnChange(el) {
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
